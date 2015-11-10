c65c70c1c947801aef3732b955b2a18a982fc735
Wolfgang Beck <beckw@telekom.de>
2015-11-10 10:04:43 +0100

    setup.py changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/ python3
+
+from distutils.core import setup
+
+setup(name="temsc",
+    description="message flow generator",
+    author="Wolfgang Beck",
+    author_email="beckw@telekom.de",
+    url="https://github.com/bewo001/temsc",
+    packages=["msc"])
