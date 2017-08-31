73ea8e396210c436051e847a7a09e4e5d8cc2d04
Wolfgang Beck <beckw@telekom.de>
2017-08-31 10:09:33 +0200

    bumped to version 1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name="temsc",
     description="message flow generator",
-    version="v1.0",
+    version="v1.1",
     author="Wolfgang Beck",
     author_email="beckw@telekom.de",
     url="https://github.com/bewo001/temsc",
